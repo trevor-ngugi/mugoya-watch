a3c3bad9ece4880b072f933471ebe1efc39e5b45
trevor ngugi <ngugitrevor@gmail.com>
2020-02-24 17:32:04 +0300

    add bootstrap

--- mugoya/settings.py
+++ mugoya/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mugoyawatch.apps.MugoyawatchConfig'
+    'mugoyawatch.apps.MugoyawatchConfig',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
