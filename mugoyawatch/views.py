111613442c47e725f812a8cad4390e7208f6dde8
trevor ngugi <ngugitrevor@gmail.com>
2020-02-24 20:58:20 +0300

    add search view function

--- mugoyawatch/views.py
+++ mugoyawatch/views.py
@@ -6,3 +6,16 @@ from .models import Profile,Neighbourhood,Posts,Business
 def home(request):
     posts=Posts.show_posts()
     return render(request, 'hood/home.html',{'posts':posts})
+
+def search_results(request):
+
+    if 'business' in request.GET and request.GET["business"]:
+        search_term = request.GET.get("business")
+        searched_business = Business.search_business(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'hood/search.html',{"message":message,"business": searched_business})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'hood/search.html',{"message":message})
