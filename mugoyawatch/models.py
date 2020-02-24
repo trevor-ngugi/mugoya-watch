b4f2f46d6e68e398aeee5f8af633fe4df0b96b48
trevor ngugi <ngugitrevor@gmail.com>
2020-02-24 19:24:41 +0300

    add profile ,business,neighbourhood model

--- mugoyawatch/models.py
+++ mugoyawatch/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+
+class Profile(models.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    username = models.CharField(max_length =30)
+    identification_card =  models.IntegerField(default=0)
+    neighbourood = models.CharField(max_length =30)#foreign key
+    bio = models.TextField()
+    email = models.EmailField()
+    contact =  models.IntegerField(default=0)
+
+
+class Business(models.Model):
+    business_name = models.CharField(max_length =30)
+    business_descrpition = models.TextField()
+    user = models.CharField(max_length =30)#foreign key
+    neighbourood = models.CharField(max_length =30)#foreign key
+    business_email = models.EmailField()
+
+
+class Neighbourood(models.Model):
+    neighnourhood_name = models.CharField(max_length =30)
+    location = models.CharField(max_length =30)
+    house_number =  models.IntegerField(default=0)
+    #admin foreging key
