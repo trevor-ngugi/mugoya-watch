cc30f1e5471cdb1053a38235078486bb9192f0a5
trevor ngugi <ngugitrevor@gmail.com>
2020-02-24 20:23:41 +0300

    registered the models in aadmin page

--- mugoyawatch/admin.py
+++ mugoyawatch/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile,Neighbourhood,Posts,Business
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Neighbourhood)
+admin.site.register(Posts)
+admin.site.register(Business)
