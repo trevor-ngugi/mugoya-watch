75fd3fa6363542cac54b076b7e010fe6e9249ece
trevor ngugi <ngugitrevor@gmail.com>
2020-02-26 11:17:10 +0300

    add login form template

--- mugoyawatch/views.py
+++ mugoyawatch/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse
 from .models import Profile,Neighbourhood,Posts,Business
+from django.contrib.auth.decorators import login_required.
 
 # Create your views here.
+
+@login_required(login_url='/accounts/login/')
 def home(request):
     posts=Posts.show_posts()
     return render(request, 'hood/home.html',{'posts':posts})
 
+
+
+@login_required(login_url='/accounts/login/')
 def search_results(request):
 
     if 'business' in request.GET and request.GET["business"]:
