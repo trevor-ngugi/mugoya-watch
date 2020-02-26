b7808313da4f0d28c24740246b3b9253f9e636f7
trevor ngugi <ngugitrevor@gmail.com>
2020-02-26 11:19:43 +0300

    edit navbar

--- mugoyawatch/views.py
+++ mugoyawatch/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse
 from .models import Profile,Neighbourhood,Posts,Business
-from django.contrib.auth.decorators import login_required.
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
